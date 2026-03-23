993b41f45358c7f34c8607cd6a0ee59a65113035
Brijesh Arumuganainar <110632170+16Brijesh10@users.noreply.github.com>
2026-03-23 17:06:06 +0530

    Document processore script updated

---
+++ app/services/document_processor.py
@@ -0,0 +1,8 @@
+from utils.loader import extract_text
+from utils.chunking import chunk_text
+from services.vector_store import store_chunks
+
+def process_document(filename, content):
+    text = extract_text(filename, content)
+    chunks = chunk_text(text)
+    store_chunks(chunks, filename)
