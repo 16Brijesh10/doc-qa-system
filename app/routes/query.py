fd6542812890a5c3187c16e018369887be68d641
Brijesh Arumuganainar <110632170+16Brijesh10@users.noreply.github.com>
2026-03-24 23:14:53 +0530

    Deleted app/routes directory

--- app/routes/query.py
+++
@@ -1,9 +0,0 @@
-#Query Api
-from fastapi import APIRouter
-from services.query_engine import get_answer
-
-router = APIRouter()
-
-@router.post("/query")
-def query(q: str):
-    return get_answer(q)
