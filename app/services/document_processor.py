ea119c3a80fcc866ecd3aaac38be084d4a4fccb9
Brijesh Arumuganainar <110632170+16Brijesh10@users.noreply.github.com>
2026-03-24 23:15:04 +0530

    Deleted app/services directory

--- app/services/document_processor.py
+++
@@ -1,9 +0,0 @@
-#Document Processing
-from utils.loader import extract_text
-from utils.chunking import chunk_text
-from services.vector_store import store_chunks
-
-def process_document(filename, content):
-    text = extract_text(filename, content)
-    chunks = chunk_text(text)
-    store_chunks(chunks, filename)
